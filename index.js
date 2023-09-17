3828ad334c31f7cb4dad415336e449ebe0f3cb4c
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-09-17 16:32:08 +0700

    feat: setup morgan

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config/config.env"});
+const process = require("process");
+const morgan = require("morgan");
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("<h1>InTask Backend</h1>");
