0136421b6036fc8bf0456a7977fcfd2443f65c23
Nc Galih <encegalih@gmail.com>
2023-09-19 15:23:29 +0700

    feat: add property contributor on project model

--- models/project.js
+++ models/project.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 const projectSchema = new mongoose.Schema({
   // TODO: Add project properties
+  contributors: [{
+    type: String,  //contributor's username
+    ref: 'User'
+  }]
 });
 
 module.exports = mongoose.model("Project", projectSchema);
