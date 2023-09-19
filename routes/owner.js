b498ffca5aff4643d679aba0cafada495b1fc24b
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-09-19 09:49:31 +0700

    feat: add to dos for owner route

---
+++ routes/owner.js
@@ -0,0 +1,24 @@
+const ownerRouter = require("express").Router();
+
+
+// TODO: Edit project
+
+
+
+// TODO: Delete project
+
+
+
+// TODO: Add contributor
+
+
+
+// TODO: Remove contributor
+
+
+
+// TODO: Share / change project privacy
+
+
+
+module.exports = ownerRouter;
