cd436f6e16f0c251abc5d286a1f17ecd634702eb
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-09-22 07:08:44 +0700

    feat: setup required packages
    Installed:
    - jsonwebtoken
    - body-parser
    - cookie-parser
    - bcrypt
    - cors

--- index.js
+++ index.js
@@ -2,13 +2,32 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 
+// ? Dotenv config
 dotenv.config({ path: "./config/config.env"});
 const process = require("process");
+
+// ? Morgan logger config
 const morgan = require("morgan");
 app.use(morgan("dev"));
 
+// ? Body parser config
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// ? Cookie parser config
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+// ? CORS config
+const cors = require("cors");
+app.use(cors({origin: "*"}));
+
+// ? DB Connection
 const connectDB = require("./config/connectDB");
 connectDB(process.env.MONGO_URI);
+
+// * Start of routes
 
 app.get("/", (req, res) => {
   res.send("<h1>InTask Backend</h1>");
@@ -19,6 +38,8 @@ app.use("/users", userRouter);
 
 const ownerRouter = require("./routes/owner");
 app.use("/owners", ownerRouter);
+
+// * End of routes
 
 const port = process.env.API_PORT;
 app.listen(port, () => {
